3f4125fbd80be4bc0c36fb8749f339fc33f26c39
James Armstead <armsteadj1@gmail.com>
2019-03-07 18:11:34 -0600

    ordering the races

--- app/seralizers/team_with_results_uci_racer_serializer.rb
+++ app/seralizers/team_with_results_uci_racer_serializer.rb
@@ -2,7 +2,7 @@ class TeamWithResultsUciRacerSerializer < TeamUciRacerSerializer
   attribute :results
 
   def results
-      object.race_results.map{|r| {
+      object.race_results.sort_by{|e| e.id}.map{|r| {
           :competition_name => r.competition_name,
           :category => r.category,
           :place => r.place,
